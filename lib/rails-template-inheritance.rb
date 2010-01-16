24ae8515532ab686271be2ddefda913aaff0a443
Jakub Stastny aka Botanicus <knava.bestvinensis@gmail.com>
2010-01-16 23:14:21 +0000

    Helpers and instance variables work in templates now

--- lib/rails-template-inheritance.rb
+++ lib/rails-template-inheritance.rb
@@ -6,13 +6,14 @@ end
 
 require "tilt"
 require "rango/mixins/render"
+require "action_view/helpers"
 
 # Rango setup
 Tilt.register "erb", Tilt::ErubisTemplate # extension "erb" => erubis template
 Rango.logger = Rails.logger
 
 # Rango::Template has just getter, because then it holds just one object,
-# so if you create a reference, it will just work.
+# so if you create a reference to, it will just work.
 # You can use Rango::Template.template.paths.clear.unshift(path)
 module Rango
   class Template
@@ -28,7 +29,22 @@ module ActionController
       Rango::Template.template_paths = @template.view_paths
       path = template_path.respond_to?(:path_without_format_and_extension) ? template_path.path_without_format_and_extension : template_path
       logger.info("Rendering #{path}" + (status ? " (#{status})" : '')) if logger
-      render_for_text Rango::RenderMixin.render(template_path.template_path, self, locals), status
+      render_for_text Rango::RenderMixin.render(template_path.template_path, self.scope, self.locals), status
+    end
+
+    def locals
+      @locals ||= {request: request}
+    end
+
+    def scope
+      self.dup.tap do |scope|
+        scope.extend(self.class.master_helper_module)
+        ActionView::Helpers.constants.grep(/Helper$/).each do |name|
+          helper = ActionView::Helpers.const_get(name)
+          scope.extend(helper)
+          scope.instance_variable_set(:@controller, self)
+        end
+      end
     end
   end
 end
--- rails-template-inheritance.gemspec
+++ rails-template-inheritance.gemspec
@@ -1,19 +1,22 @@
 #!/usr/bin/env gem build
 # encoding: utf-8
 
+require "base64"
+
 Gem::Specification.new do |s|
   s.name = "rails-template-inheritance"
-  s.version = "0.0.1"
+  s.version = "0.0.2"
   s.authors = ["Jakub Šťastný aka Botanicus"]
   s.homepage = "http://github.com/botanicus/rails-template-inheritance"
   s.summary = "Template inheritance provided by Rango in Rails!"
   s.description = "" # TODO: long description
   s.cert_chain = nil
-  s.email = ["knava.bestvinensis", "gmail.com"].join("@")
+  s.email = Base64.decode64("c3Rhc3RueUAxMDFpZGVhcy5jeg==\n")
   s.has_rdoc = true
 
   # files
-  s.files = Dir.glob("{lib,spec}/**/*") + Dir["*"]
+  s.files = `git ls-files`.split("\n")
+
   s.require_paths = ["lib"]
 
   # Ruby version
